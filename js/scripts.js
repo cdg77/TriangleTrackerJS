cd13cc61b9990de610273264e4b64072c9afeedf
Yelena Nedelko & Carlo DeGregorio <student@epicodus.com>
2015-09-08 14:02:44 -0700

    Add first passing spec and code to check if shape has three sides.

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var triangleTest = function(sideA, sideB, sideC) {
+  return true;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangleTest', function() {
+  it("true for a shape that has three sides", function() {
+    expect(triangleTest(1,2,3)).to.equal(true);
+  });
+});
